39aa6ddb5d6f5606765226f9bcc87a30d344aab2
diegorufe <diegorufe@gmail.com>
2021-01-05 07:17:38 +0100

    Firt commit

---
+++ src/main/java/com/rfboot/RfBootApplication.java
@@ -0,0 +1,13 @@
+package com.rfboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RfBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RfBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rfboot/RfBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rfboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RfBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
